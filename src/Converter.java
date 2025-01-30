a7c612c60b5ede65e23ffdab47b685f6a9edede3
segundoc361 <126724727+segundoc361@users.noreply.github.com>
2025-01-30 00:20:36 +0000

    Fourt commit

--- src/Converter.java
+++ src/Converter.java
@@ -1,1 +1,30 @@
+public class Converter {
 
+    public static void main(String[] args) {
+
+        // Convert the string strFloat into a float.
+        String strFloat = "374.25";
+        float from_strFloat = Float.parseFloat( "374.25"); // assign here
+
+        // Convert the string strInt into an integer.
+        String strInt = "1250";
+        int from_strInt = Integer.parseInt( "1250" ); // assign here
+
+        // Convert the float floatNum into an integer.
+        float floatNum = 56.715f;
+        int from_floatNum = (int) floatNum; // assign here
+
+        // Convert the integer intNum into a float.
+        int intNum = 1500;
+        float from_intNum = (float) intNum; // assign here
+
+        // Convert the double doubleNum into a string.
+        double doubleNum = 3.14159265;
+        String from_doubleNum = "" + doubleNum; // assign here
+
+
+
+        System.out.println(from_floatNum);
+        System.out.println(from_strFloat);
+    }
+}
